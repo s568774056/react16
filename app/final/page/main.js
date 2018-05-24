28bf43f2da6570aca2b67109fbf730a85ebb40e3
宋帅鹏 <568774056@qq.com>
2018-05-24 17:45:01 +0800

    react 16版本

--- app/final/page/main.js
+++ app/final/page/main.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { Button } from 'antd';
+
 class Main extends Component {
     render() {
         return (
             <div className="main-list">
-               zxaxaxaxacacascc
+                <Link to="/payApply"><Button type="primary">我的zz私人音乐坊</Button></Link>
+                <Link to="/paySelect"><Button type="primary">我的zz私人音乐坊</Button></Link>
             </div>
         );
     }
--- app/final/route.js
+++ app/final/route.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import { Router, Route, IndexRoute, IndexRedirect, browserHistory ,hashHistory} from 'react-router'
 import App from './page/app';
 import Main from './page/main';
+import PayApply from './page/payApply';
+import PaySelect from './page/paySelect';
+
 class AppRouter extends Component {
-	render() {
-	    return (
-		    <Router history={hashHistory}>
+    render() {
+        return (
+			<Router history={hashHistory}>
 				<Route path="/" component={App}>
 					<IndexRoute component={Main}/>
+					<Route path="/payApply" component={PayApply}/>
+
+					<Route path="/paySelect" component={PaySelect}/>
 				</Route>
-		    </Router>
-		);
-	}
+			</Router>
+        );
+    }
 }
 
 export default AppRouter;
